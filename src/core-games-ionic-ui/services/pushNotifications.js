28cf9f8931a1e45efea4b1f738448f219fe763f2
JTB <jtbdevelopment@gmail.com>
2015-10-28 06:44:51 -0400

    switching to newer push notification plugin

--- src/core-games-ionic-ui/services/pushNotifications.js
+++ src/core-games-ionic-ui/services/pushNotifications.js
@@ -1,20 +1,10 @@
+/*global PushNotification:false */
 'use strict';
 
+var WEEK_IN_MILLIS = 7 * 24 * 60 * 60 * 1000;
 angular.module('coreGamesIonicUi.services').factory('jtbPushNotifications',
-    ['$http', '$injector', '$rootScope', '$window', 'jtbLocalStorage', 'jtbPlayerService',
-        function ($http, $injector, $rootScope, $window, jtbLocalStorage, jtbPlayerService) {
-            var WEEK_IN_MILLIS = 7 * 24 * 60 * 60 * 1000;
-            var cordovaPush;
-            var cordovaDevice;
-            try {
-                if ($window.location.href.indexOf('file:') === 0) {
-                    cordovaPush = $injector.get('$cordovaPush');
-                    cordovaDevice = $injector.get('$cordovaDevice');
-                }
-            } catch (ex) {
-                cordovaPush = undefined;
-                cordovaDevice = undefined;
-            }
+    ['$http', '$rootScope', '$window', 'jtbLocalStorage', 'jtbPlayerService',
+        function ($http, $rootScope, $window, jtbLocalStorage, jtbPlayerService) {
 
             var deviceToken;
 
@@ -22,7 +12,7 @@ angular.module('coreGamesIonicUi.services').factory('jtbPushNotifications',
                 console.log('register token called with ' + JSON.stringify(token));
                 if (angular.isDefined(token) && token != null && token.length > 0 && token !== 'OK') {
                     console.log('Checking..');
-                    var keyBase = 'push-' + jtbPlayerService.currentPlayer().md5;
+                    var keyBase = 'pushNotification-' + jtbPlayerService.currentPlayer().md5;
                     var keyToken = keyBase + '-token';
                     var keyRegistered = keyBase + '-last';
                     var lastToken = jtbLocalStorage.get(keyToken, '');
@@ -48,88 +38,59 @@ angular.module('coreGamesIonicUi.services').factory('jtbPushNotifications',
                 }
             }
 
-            function handleAndroidEvent(event, notification) {
-                switch (notification.event) {
-                    case 'registered':
-                        if (notification.regid.length > 0) {
-                            console.log('registered cb ' + notification.regid);
-                            registerToken(notification.regid);
-                        } else {
-                            console.error('bad registered cb ' + JSON.stringify(notification));
-                        }
-                        break;
-                    case 'message':
-                        console.log('Received push message ' + JSON.stringify(notification));
-                        break;
-                    case 'error':
-                        console.error('Received error message ' + JSON.stringify(notification));
-                        break;
-                    default:
-                        console.error('Received unknown message ' + JSON.stringify(notification));
-                        break;
-                }
+            function handleNotification(notification) {
+                console.log(JSON.stringify(notification));
             }
 
-            function handleIOSEvent(event, notification) {
-                if (angular.isDefined(notification)) {
-                    if (angular.isDefined(notification.alert)) {
-                        navigator.notification.alert(notification.alert);
-                    }
-
-                    if (angular.isDefined(event.sound)) {
-                        //  TODO play sound
-                        console.log('play sound' + JSON.stringify(event.sound));
-                    }
-
-                    if (angular.isDefined(notification.badge)) {
-                        cordovaPush.setBadgeNumber(notification.badge).then(function (result) {
-                            // Success!
-                        }, function (err) {
-                            //  TODO
-                        });
-                    }
-                }
+            function handleError(error) {
+                console.log('error in push ' + JSON.stringify(error));
             }
 
-            $rootScope.$on('$cordovaPush:notificationReceived', function (event, notification) {
-                //console.log('notification event ' + JSON.stringify(event));
-                console.log('notification notification ' + JSON.stringify(notification));
-                if (angular.isDefined(notification) && angular.isDefined(notification.event)) {
-                    handleAndroidEvent(event, notification);
-                } else {
-                    handleIOSEvent(event, notification);
+            function handleRegistration(data) {
+                console.log('registration event ' + JSON.stringify(data));
+                registerToken(data.registrationId);
+            }
+
+            var pushNotification, pushInstance;
+            try {
+                if ($window.location.href.indexOf('file:') === 0 && angular.isDefined($window.PushNotification)) {
+                    pushNotification = PushNotification;
                 }
-            });
+            } catch (ex) {
+                pushNotification = undefined;
+            }
 
             $rootScope.$on('playerLoaded', function () {
                 $http.get('/api/notifications/senderID').success(function (id) {
-                    if (angular.isDefined(cordovaPush) && angular.isDefined(cordovaDevice)) {
+                    if (angular.isDefined(pushNotification) && id !== 'NOTSET') {
                         var config;
-                        var platform = cordovaDevice.getPlatform();
-                        if (platform === 'android' ||
-                            platform === 'Android' ||
-                            platform === 'amazon-fireos') {
-                            config = {
-                                senderID: id
-                            };
-                        } else {
-                            //  TODO - review
-                            config = {
+                        //  TODO
+                        config = {
+                            android: {
+                                senderId: id,
+                                sound: false,
+                                vibrate: true
+                            },
+                            ios: {
+                                senderId: id,
+                                alert: true,
                                 badge: true,
                                 sound: false,
-                                alert: true
-                            };
-                        }
-                        cordovaPush.register(config).then(function (token) {
-                            console.log('Push Token ' + token);
-                            registerToken(token);
-                        }, function (error) {
-                            console.log('Error registering ' + JSON.stringify(error));
-                            //  TODO
-                        });
+                                clearBadge: true
+                            },
+                            windows: {
+
+                            }
+                        };
+                        pushInstance = pushNotification.init(config);
+                        pushInstance.on('registration', handleRegistration);
+                        pushInstance.on('notification', handleNotification);
+                        pushInstance.on('error', handleError);
+                    } else {
+                        console.log('No pushNotification defined');
                     }
                 }).error(function (error) {
-                    console.log('Not able to get senderID');
+                    console.log('Not able to get senderID ' + JSON.stringify(error));
                     //  TODO
                 });
             });
