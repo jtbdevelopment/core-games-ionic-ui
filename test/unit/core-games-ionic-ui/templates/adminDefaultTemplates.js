aaef51e9ade60c55c39e75629418ff15bd86d733
JTB Development <jtbdevelopment@gmail.com>
2016-12-10 19:33:32 -0500

    initial work for admin screens to core

---
+++ test/unit/core-games-ionic-ui/templates/adminDefaultTemplates.js
@@ -0,0 +1,24 @@
+describe('', function () {
+
+    beforeEach(function () {
+        module('coreGamesBootstrapUi.templates');
+    });
+
+
+    var $templateCache;
+    beforeEach(inject(function (_$templateCache_) {
+        $templateCache = _$templateCache_;
+    }));
+
+    it('verify admin-switch-player.html registered', function() {
+        expect($templateCache.get('templates/core-ionic/admin/admin-switch-player.html')).toBeDefined();
+    });
+
+    it('verify admin-stats.html registered', function() {
+        expect($templateCache.get('templates/core-ionic/admin/admin-stats.html')).toBeDefined();
+    });
+
+    it('verify admin.html registered', function() {
+       expect($templateCache.get('templates/core-ionic/admin/admin.html')).toBeDefined();
+    });
+});
